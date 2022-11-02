0617c93cf185ec2917b931eb6891fa7525a3740f
rick <rickdev564@gmail.com>
2022-11-02 21:10:37 +0600

    3rd

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            Helluuu
-        </div>
+        <nav className='header-nav'>
+            <img src={logo} alt="" />
+        </nav>
     );
 };
 
